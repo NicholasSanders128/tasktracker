976990e11f8445abfdfe02a71fcb96581ab38e99
Nicholas Sanders <Stewie1288@gmail.com>
2022-01-09 11:44:57 -0800

    Tasks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
-        <header>
+        <header className='Header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
@@ -14,6 +19,11 @@ Header.defaultProps = {
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+    color: 'red', 
+    backgroundColor: 'black' 
 }
 
 export default Header
